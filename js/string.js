51a5042b0e76b8995a35c47038bb9a942a81fdf9
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-29 10:17:44 -0600

    reverse a string

---
+++ js/string.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let string = "my bonny lies over the ocean";
+let newString = "";
+
+for (let i = string.length - 1; i >= 0; i--){
+    newString += string[i];
+}
+console.log(string);
+console.log(newString);
+
+
